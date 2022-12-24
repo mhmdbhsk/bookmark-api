f472e2128a3b04165085cdae463371b960314df1
mhmdbhsk <muhammadbhaska0@gmail.com>
2022-12-24 16:36:08 +0700

    feat: initial commit

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,99 @@
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import {
+  CreateBookmarkDto,
+  EditBookmarkDto,
+} from './dto';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+
+  getBookmarks(userId: number) {
+    return this.prisma.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  getBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ) {
+    return this.prisma.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId,
+      },
+    });
+  }
+
+  async createBookmark(
+    userId: number,
+    dto: CreateBookmarkDto,
+  ) {
+    const bookmark =
+      await this.prisma.bookmark.create({
+        data: {
+          userId,
+          ...dto,
+        },
+      });
+
+    return bookmark;
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {
+    const bookmark =
+      await this.prisma.bookmark.findUnique({
+        where: {
+          id: bookmarkId,
+        },
+      });
+
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException(
+        'Bookmark not found or denied',
+      );
+
+    return this.prisma.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ) {
+    const bookmark =
+      await this.prisma.bookmark.findUnique({
+        where: {
+          id: bookmarkId,
+        },
+      });
+
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException(
+        'Bookmark not found or denied',
+      );
+
+    await this.prisma.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  prisma,
+  PrismaClient,
+} from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  cleanDb() {
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
+}
